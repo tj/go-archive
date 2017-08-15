620d4ac08dcde19d96bf6812e60a75c981df403c
TJ Holowaychuk <tj@vision-media.ca>
2017-08-15 11:19:36 -0700

    add test for negating from *

--- archive_filter_test.go
+++ archive_filter_test.go
@@ -132,3 +132,37 @@ up.json mode=-rw-r--r-- size=0
 
 	assert.Equal(t, expected, s)
 }
+
+func TestFilterPatterns_allNegated(t *testing.T) {
+	os.Chdir("testdata/node")
+	defer os.Chdir("../..")
+
+	patterns := strings.NewReader(`
+*
+!up.json
+!.something/**
+`)
+
+	f, err := archive.FilterPatterns(patterns)
+	assert.NoError(t, err, "filter")
+
+	var buf bytes.Buffer
+	a := archive.NewZip(&buf).WithFilter(f)
+	assert.NoError(t, a.Open(), "open")
+	assert.NoError(t, a.AddDir("."), "add dir")
+	assert.NoError(t, a.Close(), "close")
+
+	dir := unzip(t, &buf)
+	s, err := tree(dir)
+	assert.NoError(t, err, "tree")
+
+	expected := `.something mode=drwxr-xr-x
+.something/bar mode=drwxr-xr-x
+.something/bar/baz mode=drwxr-xr-x
+.something/bar/baz/something mode=-rw-r--r-- size=0
+.something/foo mode=-rw-r--r-- size=0
+up.json mode=-rw-r--r-- size=0
+`
+
+	assert.Equal(t, expected, s)
+}
